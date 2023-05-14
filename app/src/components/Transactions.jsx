7106b6135a2896c3fa59d202ae1d1fce15c3856b
bouslama-hamza <ham.bousa98@gmail.com>
2023-05-14 15:14:55 +0100

    Modify Files

---
+++ app/src/components/Transactions.jsx
@@ -0,0 +1,10 @@
+
+const Transactions = () => {
+  return (
+    <div className="sm:bg-pink-300 md:bg-red-400 lg:bg-blue-400 xl:bg-yellow-400 2xl:bg-green-400" >
+      Transactions
+    </div>
+  )
+}
+
+export default Transactions
