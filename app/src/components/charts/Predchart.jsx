2d0013fd4dc9ddad712f7b4200ae4583bdf693c6
bouslama-hamza <ham.bousa98@gmail.com>
2023-05-27 22:47:27 +0100

    Finish Project

--- app/src/components/charts/Predchart.jsx
+++ app/src/components/charts/Predchart.jsx
@@ -1,8 +1,5 @@
 import React, {useState , useEffect ,useContext} from 'react'
 import ReactApexChart from 'react-apexcharts';
-import Papa from 'papaparse';
-import * as tf from '@tensorflow/tfjs';
-import { saveAs } from 'file-saver';
 
 const Predchart = () => {
   
@@ -12,58 +9,40 @@ const Predchart = () => {
         data: [],
       },
     ]);
-        
-    const fetchData = async () => {
-      const response = await fetch('/src/assets/data/Solana-data.csv');
-      const csv = await response.text();
-      const results = Papa.parse(csv, { header: true });
-      const rows = results.data;
-      const prices = rows.map((row) => Number(row.Price));
-      const dates = rows.map((row) => new Date(row.Date));
-    
-      const price_prediction = [];
-      for (let index = 0; index < 12; index++) {
-        price_prediction.push([prices[prices.length - index - 1]]);
-      }
-    
-      const prediction = await loadModel([price_prediction]);
-      const next_day = new Date(
-        dates[dates.length - 1].getTime() + 24 * 60 * 60 * 1000
-      ).toLocaleDateString('en-GB', {
-        day: 'numeric',
-        month: 'short',
-        year: '2-digit',
-      }).replace(/ /g, '-');
-      
 
-      const new_row = [next_day, prediction];
-      // send data to the server
-      fetch('http://localhost:5000/api/data', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(new_row),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Success:', data);
-        }
-        )
-        .catch((error) => {
-          console.error('Error:', error);
-        }
-        );
-
-    }
-
-    
-    
     useEffect(() => {
-      fetchData();
-    }, []);
-       
+      const fetchData = async () => {
+        try {
+          const response = await fetch('http://localhost:3000/api/data', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const data = await response.json();
 
+          const formattedData = data.map((item) => [
+            new Date(item.Date).getTime(),
+            Number(item.Price),
+          ]);
+          setSeries([
+            {
+              name: 'STOCK ABC',
+              data: formattedData,
+            },
+          ]);
+
+        } catch (error) {
+          console.log('Error:', error.message);
+        }
+      };
+      fetchData();
+
+      // Fetch data every 3 seconds
+      const interval = setInterval(fetchData, 6000);
+      return () => clearInterval(interval);
+    }, []);    
+       
     const options = {
         chart: {
           id: 'area-datetime',
@@ -109,13 +88,4 @@ const Predchart = () => {
         </>
     )
 }
-
-const loadModel = async (input) => {
-  const model = await tf.loadLayersModel('/src/assets/tfjs_model/model.json');
-  const scaled_input = tf.tensor(input).div(255.0);
-  const prediction = model.predict(scaled_input);
-  const realPrediction = prediction.mul(255.0);
-  return Number(realPrediction.dataSync()[0]).toFixed(3);
-}
-
 export default Predchart
