491560edac5d255e46c9895198eb089f5189aacb
Dimokrati <houssamdimokrati@gmail.com>
2023-05-29 15:30:40 +0100

    remove personal info

--- Smart-Contact/hardhat.config.js
+++ Smart-Contact/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/zYtlq26CXtT6c9fdA1DC9h74HSzS1t7G',
-      accounts: ['657654f7cb48574b6071dc6d1a5bf24324a0bd7b22bc81c317cd112d361ae3b1'], //metamask wallet privatekey
+      accounts: [''], //metamask wallet privatekey 
     },
   },
 };
